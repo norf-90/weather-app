0f28ff56a524d0a1098dd694569a46ae7b3191b4
Oleksandr Shmatko <blacknorfolk@gmail.com>
2023-08-04 09:37:24 +0300

    fix outDir

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
+  build: {
+    outDir: '../dist',
+  },
   root: 'src',
   base: '/weather-app/',
   plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
