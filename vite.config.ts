45fe263188037876ea67508e35501f18d50ab6c6
Oleksandr Shmatko <blacknorfolk@gmail.com>
2023-08-04 12:03:28 +0300

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
-  build: {
-    outDir: './dist',
-  },
-  root: './src',
+  // build: {
+  //   outDir: './dist',
+  // },
+  // root: './src',
   // base: '/weather-app/',
   plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
 });
